b1bab82c893b54fbec8481bdba1675b603c1febf
xrfang <xrfang@gmail.com>
2017-08-11 11:10:17 +0800

    Saved per-user sql history

--- src/restique/history.go
+++ src/restique/history.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"crypto/md5"
+	"encoding/json"
 	"fmt"
+	"os"
+	"path"
 	"regexp"
 	"sort"
 	"strings"
@@ -25,6 +28,37 @@ type MfuCache struct {
 
 func (c *MfuCache) Initialize() {
 	c.entries = make(map[string]map[string]CacheEntry)
+}
+
+func (c MfuCache) Load(user string) {
+	c.Lock()
+	defer c.Unlock()
+	defer func() {
+		if e := recover(); e != nil {
+			c.entries[user] = make(map[string]CacheEntry)
+		}
+	}()
+	f, err := os.Open(path.Join(rc.HIST_PATH, user+".json"))
+	assert(err)
+	defer f.Close()
+	dec := json.NewDecoder(f)
+	var entries map[string]CacheEntry
+	assert(dec.Decode(&entries))
+	c.entries[user] = entries
+}
+
+func (c MfuCache) Save(user string, entries map[string]CacheEntry) {
+	c.Lock()
+	c.entries[user] = entries
+	c.Unlock()
+	f, err := os.Create(path.Join(rc.HIST_PATH, user+".json"))
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	enc := json.NewEncoder(f)
+	enc.SetIndent("", "    ")
+	enc.Encode(entries)
 }
 
 func (c MfuCache) Get(user, sql string) CacheEntry {
@@ -54,9 +88,7 @@ func (c MfuCache) Get(user, sql string) CacheEntry {
 			cnt = 1
 		}
 	} else {
-		c.Lock()
-		c.entries[user] = make(map[string]CacheEntry)
-		c.Unlock()
+		c.Load(user)
 	}
 	entry.Ident = ident
 	entry.rawsql = rawsql
@@ -78,7 +110,13 @@ func (c MfuCache) Update(user string, max int) []CacheEntry {
 	entries := c.entries[user]
 	c.Unlock()
 	var res []CacheEntry
+	y, m, d := time.Now().Date()
+	today := time.Date(y, m, d, 0, 0, 0, 0, time.Local)
 	for _, ce := range entries {
+		if ce.LastUse.Before(today) {
+			ce.UseCount -= 1
+			ce.LastUse = today
+		}
 		res = append(res, ce)
 	}
 	sort.Slice(res, func(i, j int) bool {
@@ -97,9 +135,7 @@ func (c MfuCache) Update(user string, max int) []CacheEntry {
 	for _, ce := range res {
 		entries[ce.Ident] = ce
 	}
-	c.Lock()
-	c.entries[user] = entries
-	c.Unlock()
+	c.Save(user, entries)
 	return res
 }
 
